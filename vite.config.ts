d684a685e69eefc4cd108e8fdad461a6d1da1bf8
Brew Slee <brew.slee.derby@pm.me>
2026-04-15 07:51:46 -0400

    fix base and workflow

--- vite.config.ts
+++ vite.config.ts
@@ -10,7 +10,7 @@ export default defineConfig({
     vue(),
     vueDevTools(),
   ],
-  base:'/',
+  base:'/scoreboard/',
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
